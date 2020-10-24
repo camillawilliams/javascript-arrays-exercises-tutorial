e0760d40fd4c1a95ae21b6e8df72197add6fd848
camillawilliams <camillawilliamsdu@gmail.com>
2020-10-24 22:50:59 +0000

    finding wally

--- .breathecode/resets/06.5-One-last-looping/app.js
+++ .breathecode/resets/06.5-One-last-looping/app.js
@@ -1,6 +1,3 @@
 let mySampleArray = ['Esmeralda', 'Kiko', 'Ruth', 'Lebron', 'Pedro', 'Maria', 'Lou', 'Fernando', 'Cesco', 'Bart', 'Annie'];
 
 //your code here
-for(var i = mySampleArray.length-1 ;0 <= i; i-- ) {
-    console.log(mySampleArray[i])
-}
